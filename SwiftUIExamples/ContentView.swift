4ab037d9608227c2765be23f3686bd0dd9bd977e
Aleksandar Geyman <ageyman@vmware.com>
2022-09-21 18:15:23 +0300

    Created initial Navigation.

--- SwiftUIExamples/ContentView.swift
+++ SwiftUIExamples/ContentView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    let examples = ["Animations": ["First Example", "Second Example", "Third Example"],
+                    "Second Example": ["First", "Second", "Third"]]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(Array(examples.keys.sorted(by: <)), id: \.self) { key in
+                    Section(header: Text(verbatim: key)) {
+                        ForEach(examples[key]!, id: \.self) { value in
+                            NavigationLink(value) {
+                                // TODO:
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("SwiftUI Examples", displayMode: .large)
+        }
     }
 }
