30f0cbb32111f9bbff64c6c578d43544768713f8
Aleksandar Geyman <ageyman@vmware.com>
2022-09-21 17:37:37 +0300

    Initial commit.

---
+++ SwiftUIExamples/SwiftUIExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExamplesApp.swift
+//  SwiftUIExamples
+//
+//  Created by Aleksandar Geyman on 21.09.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
